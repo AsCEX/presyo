3022316b3eb49f6e48616eb2103fbf43f5e31678
Allan Cabusora <yiu.ascex@gmail.com>
2026-05-03 11:30:03 +0800

    added webauthUtils

---
+++ src/lib/webauthnUtils.ts
@@ -0,0 +1,30 @@
+/**
+ * Converts an ArrayBuffer to a Base64URL string.
+ */
+export const bufferToBase64URL = (buffer: ArrayBuffer): string => {
+  const bytes = new Uint8Array(buffer);
+  let binary = "";
+  for (let i = 0; i < bytes.byteLength; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary)
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+};
+
+/**
+ * Converts a Base64URL string to an ArrayBuffer.
+ */
+export const base64URLToBuffer = (base64URL: string): ArrayBuffer => {
+  const base64 = base64URL.replace(/-/g, "+").replace(/_/g, "/");
+  const padLen = (4 - (base64.length % 4)) % 4;
+  const padded = base64 + "=".repeat(padLen);
+  const binary = atob(padded);
+  const buffer = new ArrayBuffer(binary.length);
+  const bytes = new Uint8Array(buffer);
+  for (let i = 0; i < binary.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return buffer;
+};
